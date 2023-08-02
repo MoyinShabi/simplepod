d9fcccc0a3a50b2d78fab2da00b702d2fbc5e37f
Moyinoluwa Shabi <shabimoyinoluwa19@gmail.com>
2023-08-02 03:33:13 +0100

    add simple pause & play of a podcast episode

--- lib/ui/components/podcast_details_body.dart
+++ lib/ui/components/podcast_details_body.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:podcast_search/podcast_search.dart';
 import 'package:solar_icons/solar_icons.dart';
 
 import 'description_text.dart';
 
-class PodcastDetailsBody extends StatelessWidget {
+class PodcastDetailsBody extends StatefulWidget {
   const PodcastDetailsBody({
     super.key,
     required this.podcast,
@@ -14,6 +15,26 @@ class PodcastDetailsBody extends StatelessWidget {
 
   final Podcast podcast;
   final Item selectedPodcast;
+
+  @override
+  State<PodcastDetailsBody> createState() => _PodcastDetailsBodyState();
+}
+
+class _PodcastDetailsBodyState extends State<PodcastDetailsBody> {
+  late AudioPlayer _player;
+  int _playingEpisodeIndex = -1;
+
+  @override
+  void initState() {
+    _player = AudioPlayer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +51,7 @@ class PodcastDetailsBody extends StatelessWidget {
                     ClipRRect(
                       borderRadius: BorderRadius.circular(10),
                       child: Image.network(
-                        selectedPodcast.artworkUrl600 ?? '',
+                        widget.selectedPodcast.artworkUrl600 ?? '',
                         fit: BoxFit.cover,
                         height: 170,
                         width: 170,
@@ -42,14 +63,14 @@ class PodcastDetailsBody extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            selectedPodcast.trackName ?? '',
+                            widget.selectedPodcast.trackName ?? '',
                             style: const TextStyle(
                               fontSize: 24,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
                           Text(
-                            selectedPodcast.artistName ?? '',
+                            widget.selectedPodcast.artistName ?? '',
                             style: const TextStyle(
                               fontSize: 11,
                               color: Colors.grey,
@@ -62,11 +83,11 @@ class PodcastDetailsBody extends StatelessWidget {
                 ),
                 const SizedBox(height: 20),
                 DescriptionTextWidget(
-                  text: podcast.description ?? '',
+                  text: widget.podcast.description ?? '',
                 ),
                 const SizedBox(height: 30),
                 Text(
-                  'All Episodes (${podcast.episodes.length})',
+                  'All Episodes (${widget.podcast.episodes.length})',
                   style: const TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.bold,
@@ -77,10 +98,10 @@ class PodcastDetailsBody extends StatelessWidget {
             ),
           ),
           SliverList.separated(
-            itemCount: podcast.episodes.length,
+            itemCount: widget.podcast.episodes.length,
             separatorBuilder: (context, index) => const Divider(),
             itemBuilder: (context, index) {
-              final episode = podcast.episodes[index];
+              final episode = widget.podcast.episodes[index];
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -89,7 +110,7 @@ class PodcastDetailsBody extends StatelessWidget {
                     leading: ClipRRect(
                       borderRadius: BorderRadius.circular(10),
                       child: Image.network(
-                        podcast.image ?? '',
+                        widget.podcast.image ?? '',
                         fit: BoxFit.cover,
                         height: 50,
                         width: 50,
@@ -122,9 +143,24 @@ class PodcastDetailsBody extends StatelessWidget {
                           ),
                         ),
                         GestureDetector(
-                          onTap: () {},
-                          child: const Icon(
-                            SolarIconsBold.playCircle,
+                          onTap: () {
+                            if (episode.contentUrl == null) return;
+                            setState(() {
+                              _playingEpisodeIndex = index;
+                            });
+                            if (_playingEpisodeIndex == index &&
+                                _player.playing) {
+                              _player.pause();
+                              _playingEpisodeIndex = -1;
+                              return;
+                            }
+                            _player.setUrl(episode.contentUrl!);
+                            _player.play();
+                          },
+                          child: Icon(
+                            _playingEpisodeIndex == index
+                                ? SolarIconsBold.pauseCircle
+                                : SolarIconsBold.playCircle,
                             size: 30,
                           ),
                         ),
